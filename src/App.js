d1faf99522701e72688f8521dab168ba11e33668
Art <Wattapol.w@hotmail.com>
2022-12-04 13:52:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const handleSubmit = () => {
+    if (!input) {
+      handleSubmit(null)
+    }
+    const inputnewitem = todos.push(input)
+    setInput(inputnewitem)
+    console.log(inputnewitem)
+    setInput('')
+    setTodos([...todos])
+  }
+  const clearBtn = () => {
+    setTodos([null])
+  }
+  console.log(todos)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Todolist container mt-5">
+        <ul class="list-group">
+          <h1>To do App</h1>
+          <button type='submit' onClick={handleSubmit}>Add</button>
+          <button type='submit' onClick={clearBtn}>clear</button>
+          <input type="text" className="mt-5" value={input} onChange={(e) => { setInput(e.target.value) }} />
+          {todos.map((item, index) => {
+            return <>
+              <input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="" aria-label="..." />
+
+              <li style={{ listStyle: "none" }}>{item}</li></>
+          })}
+
+        </ul>
+
+      </div>
+
     </div>
   );
 }
